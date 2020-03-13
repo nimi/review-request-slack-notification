d46434f488766438c6d2ae3ae835cdbbe56c1052
Nicholas Mitchell <nicholas@netlify.com>
2020-03-12 21:18:32 -0500

    More debugging

--- dist/index.js
+++ dist/index.js
@@ -856,7 +856,8 @@ function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             core.debug(`Sending notification to slack ...`);
-            core.debug(`Ignored Reviewers ${process.env.IGNORED_REVIEWERS}`);
+            const containsIgnoreReviewer = JSON.parse(process.env.IGNORED_REVIEWERS || '[]').some((reviewer) => reviewer === '@nimi');
+            core.debug(`Ignored Reviewers ${containsIgnoreReviewer ? 'yes' : 'no'}`);
             yield send_1.send();
             core.setOutput('Finished sending notification', new Date().toTimeString());
         }
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import {send} from './send'
 async function run(): Promise<void> {
   try {
     core.debug(`Sending notification to slack ...`)
+    const containsIgnoreReviewer = JSON.parse(
+      process.env.IGNORED_REVIEWERS || '[]'
+    ).some((reviewer: string) => reviewer === '@nimi')
 
-    core.debug(`Ignored Reviewers ${process.env.IGNORED_REVIEWERS}`)
+    core.debug(`Ignored Reviewers ${containsIgnoreReviewer ? 'yes' : 'no'}`)
     await send()
 
     core.setOutput('Finished sending notification', new Date().toTimeString())

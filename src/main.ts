944319ff9ff53b8f6a340c3616f18ac4fda4c6a9
Nicholas Mitchell <nicholas@netlify.com>
2020-03-12 20:27:29 -0500

    Debug pr object

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ async function run(): Promise<void> {
   try {
     core.debug(`Sending notification to slack ...`)
 
+    core.debug(
+      `GitHub object ${JSON.stringify(process.env.PULL_REQUEST || {})}`
+    )
+
     await send()
 
     core.setOutput('Finished sending notification', new Date().toTimeString())
